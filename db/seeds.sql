893718f245d46b08c20481946b7e699089d8db00
Kristoffer Keene <toffer2001@yahoo.com>
2018-01-08 19:03:04 -0700

    burger

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+
