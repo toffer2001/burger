5dcc470eecc13d84c1bb8430646d3c601583da24
Kristoffer Keene <toffer2001@yahoo.com>
2018-01-08 19:26:17 -0700

    changed server to index.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -20,7 +20,7 @@ router.post("/api/burgers", function (req, res) {
     burger.createOne([
         "burger_name", "devoured"
     ], [
-            req.body.name, req.body.devoured
+            req.body.burger_name, req.body.devoured
         ], function (result) {
             // Send back the ID of the new quote
             res.json({ id: result.insertId });
@@ -32,7 +32,7 @@ router.put("/api/burgers/:id", function (req, res) {
 
     console.log("condition", condition);
 
-    burger.update({
+    burger.updateOne({
         devoured: req.body.devoured
     }, condition, function (result) {
         if (result.changedRows == 0) {
@@ -46,8 +46,9 @@ router.put("/api/burgers/:id", function (req, res) {
 
 router.delete("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
+    console.log("condition: " + condition);
 
-    burger.delete(condition, function (result) {
+    burger.deleteOne(condition, function (result) {
         if (result.affectedRows == 0) {
             // If no rows were changed, then the ID must not exist, so 404
             return res.status(404).end();
