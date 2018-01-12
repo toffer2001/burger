4607d67ffb8398a61e61934be23de871509d8c0a
Kristoffer Keene <toffer2001@yahoo.com>
2018-01-11 21:28:58 -0700

    edited connection

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL){
-    connection = mysql.createConnection(process.env.JAWSDB_URL)
+    connection = mysql.createConnection(process.env.JAWSDB_URL || 3306)
 }else{
     connection = mysql.createConnection({
         host: "localhost",
