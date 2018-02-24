42498c1ca04216cb07d74910c9d6dd7484fa089a
Kristoffer Keene <toffer2001@yahoo.com>
2018-02-24 13:03:22 -0700

    heroku

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,13 @@
 
-DROP DATABASE IF EXISTS burgers_db;
-CREATE DATABASE burgers_db;
+-- DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE zgylzqxjfq0uh48o;
+-- CREATE DATABASE burgers_db;
 
-USE burgers_db;
-
+USE zgylzqxjfq0uh48o;
+-- USE burgers_db;
 
 CREATE TABLE burgers
+
 (
     id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar (255) NOT NULL,
